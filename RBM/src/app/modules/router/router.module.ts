91bd15f31c8e21314e2e739fbcf8887111b90583
Plinio Mourão <plinio.mourao@mereo.com.br>
2019-04-26 17:17:31 -0300

    salvando mudanças.

--- RBM/src/app/modules/router/router.module.ts
+++ RBM/src/app/modules/router/router.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from 'src/app/components/home/home.component';
 
 const appRoutes: Routes = [
-   { path: '',
-     redirectTo: '/home',
-     pathMatch: 'full'
-   }/*,
-   { path: '**', component: PageNotFoundComponent }
-   */
+  {
+    path: 'home',
+    component: HomeComponent
+  }, 
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }/*,
+  { path: '**', component: PageNotFoundComponent }
+  */
 ];
 
 @NgModule({
